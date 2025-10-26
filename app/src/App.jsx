fdeda3c797d293f1a946d1220f81ee070e364e5b
Vict0rTesla <amar1616@yahoo.com>
2025-10-26 19:22:59 +0530

    shifting to react

---
+++ app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SearchAppBar from './SearchAppBar.jsx'
+import Status from './Status.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Status />
+    </>
+  )
+}
+
+export default App
---
+++ app/src/Status.jsx
@@ -0,0 +1,12 @@
+import SearchAppBar from './SearchAppBar.jsx'
+import './Status.css'
+
+export default function Status() {
+  return (
+    <>
+      <div className="status" >
+        <SearchAppBar />
+      </div>
+    </>
+  )
+}
